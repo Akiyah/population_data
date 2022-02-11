98fe8a4e3f499461b8f08b3e832bca86eeede0be
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2022-02-11 13:39:08 +0900

    refactoring

--- lib/params.rb
+++ lib/params.rb
@@ -2,11 +2,6 @@
 
 PARAMS = [
   {
-    name: '男女別人口（各年10月1日現在）- 総人口，日本人人口（平成12年～27年）',
-    excel_url: 'https://www.e-stat.go.jp/stat-search/file-download?statInfId=000013168604&fileKind=0',
-    stat_url: 'https://www.e-stat.go.jp/stat-search/files?page=1&layout=datalist&toukei=00200524&tstat=000000090001&cycle=0&tclass1=000000090004&tclass2=000001051180&stat_infid=000013168604&cycle_facet=tclass1%3Acycle&tclass3val=0',
-    sheets: {}
-  }, {
     name: '年齢（各歳），男女別人口（各年10月1日現在）－総人口（大正9年～平成12年）',
     excel_url: 'https://www.e-stat.go.jp/stat-search/file-download?statInfId=000000090264&fileKind=0',
     stat_url: 'https://www.e-stat.go.jp/stat-search/files?page=1&layout=datalist&toukei=00200524&tstat=000000090001&cycle=0&tclass1=000000090004&tclass2=000000090005&stat_infid=000000090264&cycle_facet=tclass1%3Acycle&tclass3val=0',
@@ -100,5 +95,10 @@ PARAMS = [
         { year: 2000, rows: 13..103, columns: %w[BL BM] }
       ]
     }
+  }, {
+    name: '男女別人口（各年10月1日現在）- 総人口，日本人人口（平成12年～27年）',
+    excel_url: 'https://www.e-stat.go.jp/stat-search/file-download?statInfId=000013168604&fileKind=0',
+    stat_url: 'https://www.e-stat.go.jp/stat-search/files?page=1&layout=datalist&toukei=00200524&tstat=000000090001&cycle=0&tclass1=000000090004&tclass2=000001051180&stat_infid=000013168604&cycle_facet=tclass1%3Acycle&tclass3val=0',
+    sheets: {}
   }
 ].freeze
